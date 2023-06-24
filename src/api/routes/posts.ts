fb09125d8d7c77065c99a893f10f72b866233376
Adi Poluri <adipoluri@gmail.com>
2023-06-24 12:35:25 -0700

    removed open AI key

--- src/api/routes/posts.ts
+++ src/api/routes/posts.ts
@@ -3,6 +3,7 @@ import MessageResponse from '../../interfaces/MessageResponse';
 
 const postsRouter = express.Router();
 
+//get all posts ordered by date
 postsRouter.get<{}, MessageResponse>('/', (req, res) => {
   res.json({message: "Receive users"});
 });
